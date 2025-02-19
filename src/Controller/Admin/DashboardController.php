855f5e413532eff0dcc785d7831ac661cad2f281
bocchiarati <88000sofien@gmail.com>
2025-02-19 09:33:36 +0100

    EasyAdminBundle

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Controller\Admin\UserCrudController;
+
+#[Route('/admin', name: 'admin')]
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    #[Route('/', name: 'admin_dashboard')]
+    public function index(): Response
+    {
+//        return parent::index();
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return  $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Boutique');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+//         yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
+    }
+}
